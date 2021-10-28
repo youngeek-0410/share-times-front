21f7ef4e96664ec147f807f5f48f97a9db1e2600
lemonadern <taishi.develop@gmail.com>
2021-10-29 02:30:15 +0900

    feat: axios client

---
+++ src/services/api/client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const client = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { 'Content-Type': 'application/json' },
+  responseType: 'json',
+  timeout: 5000,
+})
