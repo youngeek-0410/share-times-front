7c960be9c9a75003b7c15b5e603b90d7bbe97681
lemonadern <taishi.develop@gmail.com>
2021-10-29 03:19:59 +0900

    add: 404 page

--- src/common/components/NotFound.tsx
+++ src/common/components/NotFound.tsx
@@ -1,5 +1,14 @@
+import { VStack, Text } from '@chakra-ui/layout'
+
+import koyoLogo from 'assets/koyo_logo.jpg'
+
 const NotFound = () => {
-  return <p>page not found.</p>
+  return (
+    <VStack spacing="20px" padding="20px">
+      <img src={koyoLogo} alt="こうよう祭の写真" width="100px" />
+      <Text color="tomato">404 Not Found</Text>
+    </VStack>
+  )
 }
 
 export default NotFound
